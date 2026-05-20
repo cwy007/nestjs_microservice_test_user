bef7427ca74c762dda1148df627a2271764b055c
chanweiyan <chanweiyan007@gmail.com>
2026-05-20 18:57:55 +0800

    fix: MessagePattern 参数测试

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @MessagePattern('sum')
+  // @MessagePattern({ cmd: 'sum' })
   sum(numArr: number[]): number {
     console.log('Received numbers:', numArr);
     return numArr.reduce((a, b) => a + b, 0);
